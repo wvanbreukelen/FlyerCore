2cf7fb8d700ab0301e27338b831f021f68c96af9
wvanbreukelen <wdownload@xs4all.nl>
2014-07-10 18:45:31 +0200

    Optimized for phpunit

--- phpunit.php
+++ phpunit.php
@@ -1,25 +1,5 @@
 <?php
 
-// Set error handling
-
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
-// Define the path constances
-
-define('DS', DIRECTORY_SEPARATOR);
-$stack = explode(DS, getcwd());
-$array = array_pop($stack);
-define('ROOT', implode(DS, $stack) . DS);
-define('APP', ROOT . 'app' . DS);
-
 // Require the composer autoloader
 
-require('../vendor/autoload.php');
-
-// Require the application's bootstrap
-
-require('../flyer/bootstrap.php');
-
-exit();
+require('vendor/autoload.php');
---
+++ tests/Filesystem/FilesystemTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class FilesystemTest extends PHPUnit_Framework_TestCase
+{
+	public function testPushAndPop()
+	{
+		$this->assertEquals(0, 0);
+	}
+}
