41f05d7e9bbffe94d84491e8078fed7948f7a678
wvanbreukelen <wdownload@xs4all.nl>
2014-07-12 13:30:12 +0200

    Added YouTube video functionaliy
    Using my wvanbreukelen\YouTubePHP package

---
+++ flyer/Web/YouTube/YouTubeEmbed.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Flyer\Components\Web\YouTube;
+
+class Embed {
+
+	protected $youtubeVideoID;
+
+	public function setYouTubeUrl($url)
+	{
+		$pieces = explode('watch?v=', $url);
+		$this->youtubeVideoID = $pieces[1];
+	}
+
+	public function getEmbedUrl()
+	{
+		return '//www.youtube.com/embed/' . $this->youtubeVideoID;
+	}
+
+	public function getYouTubeVideoID()
+	{
+		return $this->youtubeVideoID;
+	}
+}
