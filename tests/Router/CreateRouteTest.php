5ecf28f3d6ff5e6a78b76dda1c333635a896f42e
wvanbreukelen <wdownload@xs4all.nl>
2014-07-14 16:03:41 +0200

    Added some new assertions for phpunit

--- tests/Router/CreateRouteTest.php
+++ tests/Router/CreateRouteTest.php
@@ -3,6 +3,7 @@
 use Flyer\Components\Router\Router;
 use Flyer\Components\Router\Route;
 use Flyer\Foundation\Events\Events;
+use Symfony\Component\HttpFoundation\Request;
 
 class CreateRouteTest extends PHPUnit_Framework_TestCase
 {
@@ -13,14 +14,33 @@ class CreateRouteTest extends PHPUnit_Framework_TestCase
 
 		$router->setRequest(array('method' => "GET", 'path' => 'test'));
 
+		$this->assertTrue(is_array($router->getRequest()));
+
 		$router->addRoute("GET", 'test', function() {
 			return 'Hello World';
 		});
 
+		$router->addRoute("GET", 'test2', function() {
+			return 'Hello World2';
+		});
+
+		$router->addRoute("POST", 'test', function() {
+			return 'Hello World3';
+		});
+
 		$router->route();
 
-		$this->assertEquals('Hello World', Events::trigger('application.route'));
+		$event = Events::trigger('application.route');
 
+		$this->assertEquals('Hello World', $event);
+		$this->assertFalse('Hello World2' == $event);
+		$this->assertFalse('Hello World3' == $event);
+		
+
+
+
+		$router->setRequest(Request::createFromGlobals());
+		$this->assertTrue(is_array($router->getRequest()));
 
 	}
 }
