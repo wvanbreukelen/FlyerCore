e26a0de7e0ec818aedf9dfa5e66ee97d4e463320
wvanbreukelen <wdownload@xs4all.nl>
2014-10-25 23:20:12 +0200

    Added SSH support to the Service Provider

--- flyer/Server/ServerServiceProvider.php
+++ flyer/Server/ServerServiceProvider.php
@@ -4,11 +4,13 @@ namespace Flyer\Components\Server;
 
 use Flyer\Foundation\ServiceProvider;
 use Flyer\Components\Server\FTP\Ftp;
+use Flyer\Components\Server\SSH\Ssh;
 
 class ServerServiceProvider extends ServiceProvider
 {
 	public function register()
 	{
 		$this->share('server.ftp', new Ftp());
+		$this->share('server.ssh', new Ssh());
 	}
 }
