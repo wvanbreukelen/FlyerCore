b25fff39465de9013bde3f4b57072fb96d070f11
wvanbreukelen <wiebe.van.breukelen@hotmail.nl>
2016-06-30 01:10:03 +0200

    Phpunit bootstrap update

--- phpunit.php
+++ phpunit.php
@@ -1,7 +1,28 @@
 <?php
 
+define('AUTOLOADER_LOC', '../vendor/autoload.php');
+define('BOOTSTRAP_LOC', '../bootstrap/bootstrap.php');
+
 /**
- * Load the composer autoloader
+ * Initialize application (fatal) error handling
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+
+
+/**
+ * Check if the composer autoloader file exists
  */
 
-require('vendor/autoload.php');
+if (!file_exists(AUTOLOADER_LOC))
+{
+    throw new RuntimeException("Composer autoloader was not found, path: " . AUTOLOADER_LOC);
+}
+
+/**
+ * Require the composer autoloader
+ */
+
+require(AUTOLOADER_LOC);
--- tests/Router/CreateRouteTest.php
+++ tests/Router/CreateRouteTest.php
@@ -3,30 +3,73 @@
 use Flyer\Components\Router\Router;
 use Flyer\Components\Router\Route;
 use Flyer\Foundation\Events\Events;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyHttpRequest;
 
 class CreateRouteTest extends PHPUnit_Framework_TestCase
 {
+
+	protected $router;
+
+	public function setUp()
+	{
+		$this->router = new Router;
+	}
+
+	public function testRouteCreation()
+	{
+		$this->assertEmpty($this->router->getRoutes());
+
+		$this->createTestRoutes();
+
+		$this->assertEquals(3, count($this->router->getRoutes()));
+
+		$this->router->removeRoute('test_1');
+		$this->router->removeRoute('test_3');
+
+		$this->assertEquals(1, count($this->router->getRoutes()));
+
+		$this->assertTrue(isset($this->router->getRoutes()['test_2']));
+	}
+
+	/**
+	 * @depends testRouteCreation
+	 */
+	public function testHandleOfClosure()
+	{
+		$this->router->generateRouteEvent('test_1');
+	}
+
+	public function testSetRequestInRouter()
+	{
+		$this->router->setRequest(new SymfonyHttpRequest);
+
+		$this->assertArrayHasKey('method', $this->router->getRequest());
+		$this->assertArrayHasKey('path', $this->router->getRequest());
+
+		$this->assertEquals(2, count($this->router->getRequest()));
+
+		if (is_array($this->router->getRequest()['method']))
+		{
+			$this->assertContains($this->router->getRequest()['method'], ['GET', 'POST', 'UPDATE', 'DELETE']);
+		}
+
+		$this->assertNull($this->router->getRequest()['method']);
+
+		$this->assertTrue(is_string($this->router->getRequest()['path']));
+	}
+
+	/**
+	 * @depends testRouteCreation
+	 */
 	public function testCreateRouteAndMatchesEvent()
 	{
+		$this->router->setRequest(array('method' => "GET", 'path' => 'test'));
 
-		$router = new Router;
+		$this->assertTrue(is_array($this->router->getRequest()));
 
-		$router->setRequest(array('method' => "GET", 'path' => 'test'));
+		$this->createTestRoutes();
 
-		$this->assertTrue(is_array($router->getRequest()));
-
-		$router->addRoute("GET", 'test', function() {
-			return 'Hello World';
-		});
-
-		$router->addRoute("GET", 'test2', function() {
-			return 'Hello World2';
-		});
-
-		$router->addRoute("POST", 'test', function() {
-			return 'Hello World3';
-		});
+		//$this->assertEquals();
 
 		/**$router->route();
 
@@ -35,12 +78,27 @@ class CreateRouteTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('Hello World', $event);
 		$this->assertFalse('Hello World2' == $event);
 		$this->assertFalse('Hello World3' == $event);
-		
+
 
 
 
 		$router->setRequest(Request::createFromGlobals());
 		$this->assertTrue(is_array($router->getRequest()));**/
 
+	}
+
+	private function createTestRoutes()
+	{
+		$this->router->addRoute("GET", 'test_1', function() {
+			return 'Hello Bob';
+		});
+
+		$this->router->addRoute("GET", 'test_2', function() {
+			return 'Hello Anne';
+		});
+
+		$this->router->addRoute("POST", 'test_3', function() {
+			return 'Hello Taylor';
+		});
 	}
 }
